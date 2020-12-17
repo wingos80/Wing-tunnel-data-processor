bf08860cebd2afd23ac27cad3b6dbf7ce91ee699
elias <elias.boegel@yahoo.de>
2020-12-17 20:24:19 +0100

    Added text file processing

--- Main.py
+++ Main.py
@@ -26,3 +26,26 @@ for i in range(len(dir2)):
 nak = timeit.default_timer()
 time = nak - nik
 print("Total Processing time:", time, "seconds (" + str(round(time/60, 2)) + " mins) \n\n")
+
+
+
+# Examples for plotting data from the text files
+
+# Plotting example of a pressure distribution (cp file)
+# Reynolds number can be accessed with data[0, y_dataset - 1]
+# Angle of Attack can be accessed with data[1, y_dataset - 1]
+#y_dataset = 2 # Column number of desired data set in cp file
+#data = read_data("D:/courses/Windtunnel test/AE2130-II G25/2D/experimental/cp_test.txt").T
+#plt.xlabel(data[0][1])
+#plt.ylabel("C_p")
+#plt.plot(data[0, 2:].astype(numpy.float, casting = "unsafe"), data[y_dataset - 1, 2:].astype(numpy.float, casting = "unsafe"))
+#plt.show()
+
+# Plotting example of data in the press file
+#x_dataset = 2 # Column number of the desired data set in the press file, for example 2 when x-axis is Angle of Attack
+#y_dataset = 4 # Column number of the desired data set in the press file, for example 4 when y-axis is lift coefficient
+#data = read_data("D:/courses/Windtunnel test/AE2130-II G25/2D/experimental/press_test.txt").T
+#plt.xlabel(data[x_dataset - 1][0])
+#plt.ylabel(data[y_dataset - 1][0])
+#plt.plot(data[x_dataset - 1, 2:].astype(numpy.float, casting = "unsafe"), data[y_dataset - 1, 2:].astype(numpy.float, casting = "unsafe"))
+#plt.show()
